151fe7ee9247ba53d77a6bfcf338b4effd341ccc
aathym <aat8001@gmail.com>
2022-08-09 15:33:30 +0530

    format code

--- Max 2nd Project/src/components/Users/AddUser.js
+++ Max 2nd Project/src/components/Users/AddUser.js
@@ -1,45 +1,54 @@
-import React, {useState} from 'react'
-import Card from '../UI/Card'
-import classes from './AddUser.module.css';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
-
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
 
-    console.log(enteredUsername, enteredAge)
+    console.log(enteredUsername, enteredAge);
 
-    setEnteredUsername('')
-    setEnteredAge('')
-    
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
-
-  function userNameChangeHandler(event){
-    setEnteredUsername(event.target.value)
+  function userNameChangeHandler(event) {
+    setEnteredUsername(event.target.value);
   }
-  function ageChangeHandler(event){
-    setEnteredAge(event.target.value)
+  function ageChangeHandler(event) {
+    setEnteredAge(event.target.value);
   }
-   console.log(classes)
+  console.log(classes);
   return (
+    <Card
+      style={{ background: "red" }}
+      onClick={addUserHandler}
+      cssClass={classes.input}
+    >
+      {/* <input className={classes.input}></input> */}
+      {enteredUsername}
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input
+          value={enteredUsername}
+          onChange={userNameChangeHandler}
+          id="username"
+          type="text"
+        />
 
+        <label htmlFor="age">User Age</label>
+        <input
+          value={enteredAge}
+          onChange={ageChangeHandler}
+          id="age"
+          type="number"
+        />
 
-      <Card style={{background: "red"}} onClick={addUserHandler} cssClass={classes.input}>
-        {/* <input className={classes.input}></input> */}
-        {enteredUsername}
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">User Name</label>
-      <input value={enteredUsername} onChange={userNameChangeHandler} id="username" type="text" />
-
-      <label htmlFor="age">User Age</label>
-      <input value={enteredAge} onChange={ageChangeHandler} id="age" type="number" />
-
-      <Button type="submit"> Add User</Button>
-    </form>
+        <Button type="submit"> Add User</Button>
+      </form>
     </Card>
   );
 };
