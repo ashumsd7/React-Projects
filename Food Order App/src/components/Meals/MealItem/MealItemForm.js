52bfe28c67593749e3885145644e5b2e955c71ff
aathym <aat8001@gmail.com>
2022-08-17 13:43:32 +0530

    pass ids

--- Food Order App/src/components/Meals/MealItem/MealItemForm.js
+++ Food Order App/src/components/Meals/MealItem/MealItemForm.js
@@ -4,7 +4,7 @@ const MealItemForm = props =>{
 
    return <form className={classes.form}>
     <Input label='Amount' input={{
-        id:'Amount',
+       id: 'amount_' + props.id,
         type:'number',
         min:'1',
         max:'5',
