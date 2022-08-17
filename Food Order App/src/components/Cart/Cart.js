b5b93337707cb70c158a3d82de665d45c9539890
aathym <aat8001@gmail.com>
2022-08-17 18:44:00 +0530

    Modal portal added

--- Food Order App/src/components/Cart/Cart.js
+++ Food Order App/src/components/Cart/Cart.js
@@ -1,32 +1,27 @@
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Susi", amount: 2, price: 11.22 }].map((item) => {
+       return <li key={item.id}>{item.name}</li>;
+      })}
+    </ul>
+  );
 
-import classes from './Cart.module.css'
-const Cart = props =>{
+  return (
+    <Modal>
+      {cartItems}
+      <div>
+        <span>Total Amount</span>
+        <span>32.67</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button"]}>Order</button>
+      </div>
+    </Modal>
+  );
+};
 
-
-    const cartItems= <ul className={classes['cart-items']}>{[
-        {id:'c1', name: 'Susi', amount:2, price:11.22}
-    ].map(item=>{<li>{item.name}</li>})}</ul>
-
-
-
-    return <div>
-        {cartItems}
-        <div>
-            <span>Total Amount</span>
-            <span>32.67</span>
-        </div>
-        <div className={classes.actions}>
-
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes['button']}>Order</button>
-
-        </div>
-      
-    </div>
-
-
-
-}
-
-
-export default Cart
+export default Cart;
---
+++ Food Order App/src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import ReactDOM from 'react-dom'
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlays = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+
+    const portalElement= document.getElementById('overlays')
+    console.log(portalElement)
+  return (
+    <Fragment>
+      {/* <Backdrop></Backdrop>
+      <ModalOverlays>{props.children}</ModalOverlays> */}
+      {ReactDOM.createPortal(<Backdrop/>,portalElement)}
+      {ReactDOM.createPortal(  <ModalOverlays>{props.children}</ModalOverlays>,portalElement)}
+     
+    </Fragment>
+  );
+};
+
+export default Modal;
