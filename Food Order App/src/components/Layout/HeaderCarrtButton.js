a8bdb73aaa659ef61fbb3a9ea4458858b800e458
aathym <aat8001@gmail.com>
2022-08-18 18:28:10 +0530

    button bump and finish project

--- Food Order App/src/components/Layout/HeaderCarrtButton.js
+++ Food Order App/src/components/Layout/HeaderCarrtButton.js
@@ -1,17 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 import classes from "./HeaderCart.module.css";
 const HeaderCartButton = (props) => {
+  const [buttonIsHighLighted, setButtonIsHighLighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
-  
+
+  const { items } = cartCtx;
 
   const numberOfCartItem = cartCtx.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    buttonIsHighLighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setButtonIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighLighted(false);
+    }, 300);
+
+    return () => {
+      // cleanup
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         {" "}
         <CartIcon />
